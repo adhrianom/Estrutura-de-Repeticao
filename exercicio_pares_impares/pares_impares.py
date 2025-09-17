120e0771df9c0065e7cda0a28806fb2359b22f41
adhrianom <adhrianocanal@gmail.com>
2025-09-16 21:05:53 -0300

    exercicio pares_impares

---
+++ exercicio_pares_impares/pares_impares.py
@@ -0,0 +1,20 @@
+pares = 0
+impares = 0
+positivos = 0
+negativos = 0
+
+for _ in range(5):
+    num = int(input())
+    if num % 2 == 0:
+        pares += 1
+    else:
+        impares += 1
+    if num >= 0:
+        positivos += 1
+    else:
+        negativos += 1
+
+print(f"{pares} valor(es) par(es)")
+print(f"{impares} valor(es) impar(es)")
+print(f"{positivos} valor(es) positivo(s)")
+print(f"{negativos} valor(es) negativo(s)")
